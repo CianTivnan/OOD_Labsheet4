8bcb0e560d053e48dce6b3641b33fcda2d862006
CianTivnan <cian.tivnan@yahoo.ie>
2021-03-03 09:49:45 +0000

    commit

--- Northwind Products/MainWindow.xaml.cs
+++ Northwind Products/MainWindow.xaml.cs
@@ -20,9 +20,49 @@ namespace Northwind_Products
     /// </summary>
     public partial class MainWindow : Window
     {
+        NORTHWNDEntities db = new NORTHWNDEntities();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //populate stock level listbox
+            lbxStock.ItemsSource = Enum.GetNames(typeof(StockLevel));
+
+            //Populate suppliers listbox
+            var query1 = from s in db.Suppliers
+                         orderby s.CompanyName
+                         select new
+                         {
+                             SupplierName = s.CompanyName,
+                             SupplierID = s.SupplierID,
+                             Country = s.Country
+                         };
+
+            lbxSuppliers.ItemsSource = query1.ToList();
+
+            //Populate countries list
+            var query2 = query1
+                .OrderBy(s => s.Country)
+                .Select(s => s.Country);
+
+            var countries = query2.ToList();
+
+            lbxCountries.ItemsSource = countries.Distinct();
+        }
+
+        private void lbxStock_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //get stock level selected
+            var query = from p in db.Products
+                        where p.UnitsInStock < 50
+                        orderby p.ProductName
+                        select p.ProductName;
+        }
     }
+
+    public enum StockLevel { Low, Normal, Overstocked };
 }
