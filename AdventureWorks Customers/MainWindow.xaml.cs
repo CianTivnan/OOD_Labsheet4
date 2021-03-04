6065047b363890a0271d70c4a12c9c094b368c6c
CianTivnan <cian.tivnan@yahoo.ie>
2021-03-04 09:57:54 +0000

    Task 2 complete

--- AdventureWorks Customers/MainWindow.xaml.cs
+++ AdventureWorks Customers/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace AdventureWorks_Customers
     /// </summary>
     public partial class MainWindow : Window
     {
+        AdventureLiteEntities db = new AdventureLiteEntities();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,12 +29,48 @@ namespace AdventureWorks_Customers
 
         private void lbxCustomers_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            string customer = lbxCustomers.SelectedItem as string;
 
+            if (customer != null)
+            {
+                var query = from o in db.SalesOrderHeaders
+                            where o.Customer.CompanyName.Equals(customer)
+                            select o;
+
+                lbxOrders.ItemsSource = query.ToList();
+            }
         }
 
         private void lbxOrders_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            int orderID = Convert.ToInt32(lbxOrders.SelectedValue);
 
+            if (orderID > 0)
+            {
+                var query = from od in db.SalesOrderDetails
+                            where od.SalesOrderID == orderID
+                            select new
+                            {
+                                ProductName = od.Product.Name,
+                                od.UnitPrice,
+                                od.UnitPriceDiscount,
+                                od.OrderQty,
+                                od.LineTotal
+                            };
+
+                dgOrderDetails.ItemsSource = query.ToList();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var query = from o in db.SalesOrderHeaders
+                        orderby o.Customer.CompanyName
+                        select o.Customer.CompanyName;
+
+            var result = query.ToList();
+
+            lbxCustomers.ItemsSource = result.Distinct();
         }
     }
 }
